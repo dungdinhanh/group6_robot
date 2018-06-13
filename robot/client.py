e018fede4141743ae2b22d1dbb83e91ffda690f3
Huy Nguyen <nqhuy1811@gmail.com>
2018-06-13 16:12:00 +0700

    robot server

--- robot/client.py
+++ robot/client.py
@@ -3,19 +3,15 @@ import json
 import time
 import threading
 
+# Dependencies for web server
+import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
 # Example of how to create the client socket and send json to the robot.
 server_socket = BluetoothSocket(RFCOMM)
 
 server_socket.connect(("D4:36:39:D1:E3.1F", 1))
-#
-# <<<<<<< HEAD
-# while True:
-#     data = {}
-#
-#     data['list_action'] = [1, 2, 3]
-#     json_data = json.dumps(data)
-# =======
-# >>>>>>> 7cf62506b441648cf98990f19f40bb6a17a68771
 
 data = {}
 
@@ -33,3 +29,46 @@ time.sleep(2)
 
 #server_socket.close()
 
+# Webserver
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        # Get size of data
+        content_length = int(self.headers['Content-Length'])
+        # Get data
+        post_data = self.rfile.read(content_length)
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        # Parse JSON data
+        requestObject = json.loads(post_data.decode('utf-8'))
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting robot server...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping robot server...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
